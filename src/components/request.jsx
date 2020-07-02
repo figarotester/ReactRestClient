0923865a92b2f6ff9ea7c53a52b5f221aa5c167c
Eric <gms_eric22@yahoo.com>
2020-07-02 17:09:41 -0600

    added axios.get and proxy

--- src/components/request.jsx
+++ src/components/request.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import UrlBar from './url_bar';
 import TabButton from './tab_button';
 import MethodSelector from './method_selector';
 import SendButton from './send_button';
 import BodyButton from './body_button';
 import HeaderButton from './header_button';
+import axios from 'axios';
 
 const StyledRequest = styled.h1`
   position: absolute;
@@ -19,80 +19,120 @@ const StyledRequest = styled.h1`
   font-size: 20px;
   line-height: 21px;
 `;
-
 const StyledMethod = styled.h1`
   position: absolute;
   width: 49px;
   height: 18px;
   left: 9px;
   top: calc(50% - 18px/2 - 130.5px);
-
   font-family: Roboto;
   font-style: normal;
   font-weight: bold;
   font-size: 14px;
   line-height: 16px;
-
   color: #000000;
 `;
-
 const StyledUrl = styled.h1`
   position: absolute;
   width: 48px;
   height: 18px;
   left: 109px;
   top: calc(50% - 18px/2 - 130.5px);
-
   font-family: Roboto;
   font-style: normal;
   font-weight: bold;
   font-size: 14px;
   line-height: 16px;
-
   color: #000000;
 `;
 
+const StyledUrlBar = styled.div`
+position: absolute;
+left: 110px;
+top: 23px;
+`;
+
 const StyledRequestWrapper = styled.section`
-  position: absolute;
-  width: 90px;
-  height: 21px;
-  left: 56px;
-  top: 16px;
+position: absolute;
+width: 90px;
+height: 21px;
+left: 56px;
+top: 16px;
 `;
 
 const RequestFrame = styled.section`
-
-  position: absolute;
-  width: 700px;
-  height: 289px;
-  left: 56px;
-  top: 51px;
-
-  background: #EEEAEA;
+position: absolute;
+width: 700px;
+height: 289px;
+left: 56px;
+top: 51px;
+background: #EEEAEA;
 `;
 
-const Request = () =>{
-  return(
-    <StyledRequestWrapper>
-      <StyledRequest>
-        Request
-      </StyledRequest>
-      <TabButton/>
-      <RequestFrame>
-        <StyledMethod>
-          Method
-        </StyledMethod>
-        <StyledUrl>
-          URL
-        </StyledUrl>
-        <UrlBar/>
-        <SendButton/>
-        <MethodSelector/>
-        <BodyButton/>
-        <HeaderButton/>
-      </RequestFrame>
-    </StyledRequestWrapper>
-  );
+class Request extends React.Component{
+constructor(props){
+super(props);
+this.state = {
+value: ''
+};
+
+this.handleChange = this.handleChange.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
 }
 
+handleChange(event){
+this.setState({value: event.target.value});
+}
+
+handleSubmit(event){
+event.preventDefault();
+axios.get(this.state.value)
+  .then(res => {
+    const data = res.data
+    console.log(data)
+    this.setState({data})
+  })
+  .catch((error) => {
+  console.log(error)
+  });
+}
+
+
+render(){
+  return(
+  <StyledRequestWrapper>
+    <StyledRequest>
+      Request
+    </StyledRequest>
+    <TabButton/>
+    <RequestFrame>
+    <StyledMethod>
+      Method
+    </StyledMethod>
+    <StyledUrl>
+      URL
+    </StyledUrl>
+    <StyledUrlBar>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="value"
+          value={this.state.value}
+          onChange={this.handleChange}
+          type="text"
+          style={{width: "330px"}}/>
+      <SendButton type="submit" value="Submit">
+        <h3>
+        Response: {this.state.value}
+        </h3>
+      </SendButton>
+    </form>
+    </StyledUrlBar>
+    <MethodSelector/>
+    <BodyButton/>
+    <HeaderButton/>
+    </RequestFrame>
+  </StyledRequestWrapper>
+  );
+  }
+  }
 export default Request
--- src/components/send_button.jsx
+++ src/components/send_button.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-
 const StyledSendButton = styled.button`
-  position: absolute;
-  width: 120px;
-  height: 31px;
-  left: 500px;
-  top: 21px;
-
-  background: #11F35E;
+position: absolute;
+width: 120px;
+height: 31px;
+left: 400px;
+top: 0px;
+background: #11F35E;
 `;
-
 const SendButton = () =>{
-  return(
-    <StyledSendButton>Send</StyledSendButton>
-  );
+return(
+<StyledSendButton>Send</StyledSendButton>
+);
 }
-
 export default SendButton
