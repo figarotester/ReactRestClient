68ea9af879b5b601ac6396208940e67f6211f5c0
Eric <gms_eric22@yahoo.com>
2020-07-07 11:52:53 -0600

    have response display in right area

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Request from './components/request';
-import Response from './components/response';
 import Collections from './components/collections';
 
 function App(){
   return(
     <div>
     <Request/>
-    <Response/>
     <Collections/>
     </div>
   );
--- client/src/components/request.jsx
+++ client/src/components/request.jsx
@@ -4,8 +4,7 @@ import TabButton from './tab_button';
 import MethodSelector from './method_selector';
 import SendButton from './send_button';
 import BodyButton from './body_button';
-import HeaderButton from './header_button';
-import axios from 'axios';
+import StatusBar from './status';
 
 const StyledRequest = styled.h1`
   position: absolute;
@@ -69,42 +68,134 @@ const RequestFrame = styled.section`
   background: #EEEAEA;
   `;
 
+const StyledResponse = styled.h1`
+  position: absolute;
+  width: 90px;
+  height: 21px;
+  left: 56px;
+  top: 40px;
+
+  font-family: Roboto;
+  font-style: italic;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 21px;
+`;
+
+const ResponseFrame = styled.section`
+  position: absolute;
+  width: 700px;
+  height: 289px;
+  left: 56px;
+  top: 75px;
+
+  background: #EEEAEA;
+`;
+
+const StyledResponseWrapper = styled.section`
+  position: absolute;
+  width: 90px;
+  height: 21px;
+  left: 0px;
+  top: 330px;
+`;
+
+const StyledStatus = styled.h1`
+  position: absolute;
+  width: 49px;
+  height: 18px;
+  left: 9px;
+  top: calc(50% - 22px/2 - 130.5px);
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+
+  color: #000000;
+`;
+
+const StyledResponseText = styled.section`
+  position: absolute;
+  width: 640px;
+  height: 130px;
+  left: 30px;
+  top: 140px;
+
+  background: white
+
+;`
+
+const StyledHeaderButton = styled.button`
+  position: absolute;
+  width: 90px;
+  height: 23px;
+  left: 350px;
+  top: 104px;
+`;
+
+/*const generateElement = (key,value) => {
+  return (
+    <div key={key} className="row">
+      <div className="col-xs-6 ins-label">{key}</div>
+      <div className="col-xs-6">{value}</div>
+    </div>
+  );
+}*/
+
+/*function generateData(data) {
+  const newData = Object.keys(data).reduce((result, currentKey) => {
+    if (typeof data[currentKey] === 'string' || data[currentKey] instanceof String) {
+      const elementToPush = generateElement(currentKey, data[currentKey]);
+      result.push(elementToPush);
+    } else {
+      const nested = generateData(data[currentKey]);
+      result.push(...nested);
+    }
+    return result;
+  }, []);
+  return newData;
+}*/
+
+
 class Request extends Component {
   constructor(props){
     super(props);
     this.state = {
     url: '',
-    response: ''
+    response: '',
+    header: ''
   };
 
   this.handleChange = this.handleChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
-}
+};
 
   handleChange = (event) => {
   this.setState({url: event.target.value});
 }
 
-handleSubmit = async (event) => {
-  event.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault();
 
-  const postbody = {proxyurl: this.state.url, proxymethod: 'get'};
-  console.log(JSON.stringify(postbody));
+    const postbody = {proxyurl: this.state.url, proxymethod: 'get'};
+    console.log(JSON.stringify(postbody));
 
-  fetch('proxy', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(postbody)
-  }).then(response => response.json())
-  .then(json => {
-    console.log('parsed json', json)
-    this.setState({response: JSON.stringify(json, null, 2)});
-  });
+    fetch('proxy', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postbody)
+    }).then(response => response.json())
+    .then(json => {
+      console.log('parsed json', json)
+      this.setState({response: JSON.stringify(json, null, 2)});
+    });
 
-}
+  }
 
 
 render(){
@@ -129,17 +220,35 @@ render(){
           onChange={this.handleChange}
           type="text"
           style={{width: "330px"}}/>
-      <SendButton type="submit" value="Submit"/>
-      <br/>
-      <br/>
-      <textarea rows={8} cols={80} value={this.state.response} onChange={this.handleChange}/>
-    </form>
+        <SendButton type="submit" value="Submit"/>
+      </form>
     </StyledUrlBar>
     <MethodSelector/>
     <BodyButton/>
-    <HeaderButton/>
+    <StyledHeaderButton>Header</StyledHeaderButton>
     </RequestFrame>
+    <StyledResponseWrapper>
+      <StyledResponse>
+        Response
+      </StyledResponse>
+      <ResponseFrame>
+        <StyledStatus>
+          Status
+        </StyledStatus>
+        <StatusBar/>
+        <BodyButton/>
+        <StyledHeaderButton>
+        Header
+      </StyledHeaderButton >
+        <StyledResponseText>
+          <textarea rows={5} cols={63} value={this.state.response} onChange={this.handleChange}/>
+        </StyledResponseText>
+      </ResponseFrame>
+      
+    </StyledResponseWrapper>
+    
   </StyledRequestWrapper>
+  
     );
   }
 }
